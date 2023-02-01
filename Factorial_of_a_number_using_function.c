436700d9c1a3ecb6f5a86fcddada9f13990bd5f5
Devarsh Sharma <devarshsharmasurat@gmail.com>
2023-02-01 09:18:09 +0530

    Create Factorial_of_a_number_using_function.c

---
+++ Factorial_of_a_number_using_function.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int fact(int n);
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    printf("Fatorial of %d is %d",n,fact(n));
+    
+    return 0;
+}
+
+int fact(int n)//control transfer function
+{
+    int i, factorial=1;
+    for (i=1;i<=n;i++)
+        factorial=factorial*i;
+    return factorial;
+}
+  
