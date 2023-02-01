eaa9160d191ca175c07307a63e2a53c7bf74f596
Devarsh Sharma <devarshsharmasurat@gmail.com>
2023-02-01 09:10:20 +0530

    Create Types_of_Functions.c

---
+++ Types_of_Functions.c
@@ -0,0 +1,17 @@
+/*
+Function Prototype:
+1. Return Value
+2. Function Call
+3. Call by Value
+4. Call by reference
+5. Function defination
+6. Arguments/Parameters
+7. Return Datatype
+
+Types of Function:
+1. Function with arguments & with return value
+2. Function with argument & without return value
+3. Function without arguments & with return value
+4. Function without arguments & without return value
+5. Recessive Function
+*/
