235106ce3732b345e1e16405fd9d0af8495b171c
Devarsh Sharma <devarshsharmasurat@gmail.com>
2023-02-01 10:52:14 +0530

    Create String.c

---
+++ String.c
@@ -0,0 +1,11 @@
+/* 
+1. Always written in " ".
+2. \0 is used to determine the end of the string.
+3. \0 is NULL character.
+4. One extra storage is required to store \0.
+5. Compiler stops reading after it encounters a space, because it think it as input termination.
+6. To read a string &str is not used in scanf. We use str.
+7. scanf("%s",str);
+8. gets(str);(only used for string) is used to enter a string with a space between the string.
+9. puts(str);(only used for string) is used for output of the string.
+*/
