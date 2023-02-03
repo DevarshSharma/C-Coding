3dcba524ace82371edb92f50e4f1b5bdd5a3496b
Devarsh Sharma <devarshsharmasurat@gmail.com>
2023-02-03 10:42:47 +0530

    Update Armstrong_Number.c

--- Armstrong_Number.c
+++ Armstrong_Number.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 int main()
 {
-    int a, b=0, c=0;
+    int a, b=0, c=0,i=0;
     scanf("%d", &a);
     int z=a;
     while (a!=0)
     {
+        a/=10;
+        ++i;
+    }
+    a=z;
+    while (a!=0)
+    {
         b=a%10;
-        c=(b*b*b) + c;
+        c=pow(b,i)+c;
         a/=10;
     }
+    
     if (z==c)
         printf("%d is an armstrong number", z);
 
